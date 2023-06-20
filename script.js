977a857021686eba9bccbdfed6a4131e7beae852
julellisg <julianellisg@gmail.com>
2023-06-19 23:41:11 -0400

    Added basic calculation functionality

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-var rowCount = 0;
-
-var acc = document.getElementsByClassName("accordion");
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+var rowCount = 1;
 
 function addRow(holderName, holderGrade, holderWeight) {
     var container = document.getElementById("container");
@@ -28,8 +14,8 @@ function addRow(holderName, holderGrade, holderWeight) {
     nameInput.type = "text";
     nameInput.id = "name";
     nameInput.name = "textbox[]";
-    if(holderName != undefined) {  // if there is a value
-        nameInput.placeholder = "ex. "+holderName;   
+    if (holderName != undefined) {  // if there is a value
+        nameInput.placeholder = "ex. " + holderName;
     }
 
     var gradeInput = document.createElement("input");
@@ -58,30 +44,98 @@ function addRow(holderName, holderGrade, holderWeight) {
     container.appendChild(form);
 }
 
+function initAccordion() {
+    var acc = document.getElementsByClassName("accordion");
+
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+}
+
+function initRows() {
+    addRow("Assignment 2", "80", "12.5");
+    addRow("Midterm", "77", "25");
+    addRow("Assignment 3", "89", "15");
+}
+
+function findLetterGrade(average) {
+    if (average >= 95)
+        return "A+";
+    else if (average >= 87 && average <= 94)
+        return "A";
+    else if (average >= 80 && average <= 86)
+        return "A-";
+    else if (average >= 77 && average <= 79)
+        return "B+";
+    else if (average >= 72 && average <= 76)
+        return "B";
+    else if (average >= 70 && average <= 71)
+        return "B-";
+    else if (average >= 67 && average <= 69)
+        return "C+";
+    else if (average >= 63 && average <= 66)
+        return "C";
+    else if (average >= 60 && average <= 62)
+        return "C-";
+    else if (average >= 57 && average <= 59)
+        return "D+";
+    else if (average >= 54 && average <= 56)
+        return "D";
+    else if (average >= 51 && average <= 53)
+        return "D-";
+    else
+        return "F";
+}
+
+function writeOutput(average, weight) {
+    var letterGrade = findLetterGrade(average);
+
+    // Retrieve the input elements
+    var outputGradeInput = document.getElementById("outputGrade");
+    outputGradeInput.value = average;
+
+    var outputWeightInput = document.getElementById("outputWeight");
+    outputWeightInput.value = weight;
+
+    if (weight < 100) {
+        var remainder = 100 - weight;
+        
+    }
+}
+
 function calcCurrentGrade() {
     var average = 0;
     var totalWeight = 0;
 
-    for(var i = 0; i <= rowCount; i++) {
+    for (var i = 1; i <= rowCount; i++) {
         var form = document.getElementById("row" + i);
 
         var gradeInput = form.querySelector("#grade");
         var grade = parseFloat(gradeInput.value);
-        
+
         var weightInput = form.querySelector("#weight");
-        var weight = parseFloat(weightInput.value);       
+        var weight = parseFloat(weightInput.value);
 
         if (isNaN(grade) || isNaN(weight))
             continue;
 
-        // Perform Calculations
-        average = average + (grade*(weight/100));
+        average = average + (grade * (weight / 100));
         totalWeight = totalWeight + weight;
     }
-    
-    console.log(`Average of ${average} and \n combined weight of ${totalWeight}`);
+
+    if (average == 0 && totalWeight == 0) return;
+
+    writeOutput(average, totalWeight);
 }
 
-addRow("Assignment 2", "80", "12.5");
-addRow("Midterm", "77", "25");
-addRow("Assignment 3", "89", "15");
+// Initialization
+initAccordion();
+initRows();
